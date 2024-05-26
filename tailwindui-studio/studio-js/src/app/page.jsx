1b50c600f1af7570bb936ff3e62761444e45a9e2
Linus Chan <chanx228@umn.edu>
2024-05-26 17:44:32 -0500

    t
    t

---
+++ tailwindui-studio/studio-js/src/app/page.jsx
@@ -0,0 +1,235 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { ContactSection } from '@/components/ContactSection'
+import { Border } from '@/components/Border'
+
+import { Container } from '@/components/Container'
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import { List, ListItem } from '@/components/List'
+import { SectionIntro } from '@/components/SectionIntro'
+import { StylizedImage } from '@/components/StylizedImage'
+import { Testimonial } from '@/components/Testimonial'
+import logoBrightPath from '@/images/clients/bright-path/logo-light.svg'
+import logoFamilyFund from '@/images/clients/family-fund/logo-light.svg'
+import logoGreenLife from '@/images/clients/green-life/logo-light.svg'
+import logoHomeWork from '@/images/clients/home-work/logo-light.svg'
+import logoMailSmirk from '@/images/clients/mail-smirk/logo-light.svg'
+import logoNorthAdventures from '@/images/clients/north-adventures/logo-light.svg'
+import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
+import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
+import logoUnseal from '@/images/clients/unseal/logo-light.svg'
+import imageLaptop from '@/images/laptop.jpg'
+import imageLinusChan from '@/images/team/linus-chan.jpg'
+import imageDriesVincent from '@/images/team/dries-vincent.jpg'
+import imageLeviPrestegard from '@/images/team/levi-prestegard.jpg'
+import { loadCaseStudies } from '@/lib/mdx'
+
+const clients = [
+  ['Phobia', logoPhobiaLight],
+  ['Family Fund', logoFamilyFund],
+  ['Unseal', logoUnseal],
+  ['Mail Smirk', logoMailSmirk],
+  ['Home Work', logoHomeWork],
+  ['Green Life', logoGreenLife],
+  ['Bright Path', logoBrightPath],
+  ['North Adventures', logoNorthAdventures],
+]
+
+function Clients() {
+  return (
+    <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
+      <Container>
+        <FadeIn className="flex items-center gap-x-8">
+          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+            We’ve worked with hundreds of amazing people
+          </h2>
+          <div className="h-px flex-auto bg-neutral-800" />
+        </FadeIn>
+        <FadeInStagger faster>
+          <ul
+            role="list"
+            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4"
+          >
+            {clients.map(([client, logo]) => (
+              <li key={client}>
+                <FadeIn>
+                  <Image src={logo} alt={client} unoptimized />
+                </FadeIn>
+              </li>
+            ))}
+          </ul>
+        </FadeInStagger>
+      </Container>
+    </div>
+  )
+}
+
+function CaseStudies({ caseStudies }) {
+  return (
+    <>
+      <SectionIntro
+        title="Pedal to your happy place"
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          We believe technology is not the answer to the world’s greatest
+          challenges. Let's just go outside and have some fun!s
+        </p>
+      </SectionIntro>
+
+
+ 
+    </>
+  )
+}
+
+function Services() {
+  return (
+    <>
+      <SectionIntro
+        eyebrow="Services"
+        title="We help you identify, explore and respond to new opportunities."
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          As long as those opportunities involve giving us money to re-purpose
+          old projects — we can come up with an endless number of those.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <div className="lg:flex lg:items-center lg:justify-end">
+          <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
+            <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
+              <StylizedImage
+                src={imageLaptop}
+                sizes="(min-width: 1024px) 41rem, 31rem"
+                className="justify-center lg:justify-end"
+              />
+            </FadeIn>
+          </div>
+          <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
+            <ListItem title="Web development">
+              We specialise in crafting beautiful, high quality marketing pages.
+              The rest of the website will be a shell that uses lorem ipsum
+              everywhere.
+            </ListItem>
+            <ListItem title="Application development">
+              We have a team of skilled developers who are experts in the latest
+              app frameworks, like Angular 1 and Google Web Toolkit.
+            </ListItem>
+            <ListItem title="E-commerce">
+              We are at the forefront of modern e-commerce development. Which
+              mainly means adding your logo to the Shopify store template we’ve
+              used for the past six years.
+            </ListItem>
+            <ListItem title="Custom content management">
+              At Studio we understand the importance of having a robust and
+              customised CMS. That’s why we run all of our client projects out
+              of a single, enormous Joomla instance.
+            </ListItem>
+          </List>
+        </div>
+      </Container>
+    </>
+  )
+}
+
+
+const team = [
+  {
+    title: 'Who we are',
+    people: [
+      {
+        name: 'Linus Chan',
+        role: 'Lead Servant / CEO',
+        image: { src: imageLinusChan },
+      },
+      {
+        name: 'Levi Prestegard',
+        role: 'Chief Biker / CEO',
+        image: { src: imageLeviPrestegard },
+      },
+ 
+    ],
+  },
+  ]
+ 
+function Team() {
+  return (
+    <Container className="mt-24 sm:mt-32 lg:mt-40">
+      <div className="space-y-24">
+        {team.map((group) => (
+          <FadeInStagger key={group.title}>
+            <Border as={FadeIn} />
+            <div className="grid grid-cols-1 gap-6 pt-12 sm:pt-16 lg:grid-cols-4 xl:gap-8">
+              <FadeIn>
+                <h2 className="font-display text-2xl font-semibold text-neutral-950">
+                  {group.title}
+                </h2>
+              </FadeIn>
+              <div className="lg:col-span-3">
+                <ul
+                  role="list"
+                  className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8"
+                >
+                  {group.people.map((person) => (
+                    <li key={person.name}>
+                      <FadeIn>
+                        <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
+                          <Image
+                            alt=""
+                            {...person.image}
+                            className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
+                          />
+                          <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
+                            <p className="font-display text-base/6 font-semibold tracking-wide text-white">
+                              {person.name}
+                            </p>
+                            <p className="mt-2 text-sm text-white">
+                              {person.role}
+                            </p>
+                          </div>
+                        </div>
+                      </FadeIn>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </FadeInStagger>
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+
+export const metadata = {
+  description:
+    'We are a development studio working at the intersection of design and technology.',
+}
+
+export default async function Home() {
+  let caseStudies = (await loadCaseStudies()).slice(0, 3)
+
+  return (
+    <>
+      <Container className="mt-24 sm:mt-32 md:mt-56">
+        <FadeIn className="max-w-3xl">
+          <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+            Bicycle tours in Minnesota.
+          </h1>
+          <p className="mt-6 text-xl text-neutral-600">
+          We're two University of Minnesota alumni who live and breathe biking. Let us be your guides to the best-kept secrets of Minneapolis and beyond, from hidden urban trails to epic up-north escapades.</p>
+
+        </FadeIn>
+      </Container>
+      <Team />
+      <CaseStudies caseStudies={caseStudies} />
+     </>
+  )
+}
+
+
+
---
+++ tailwindui-studio/studio-js/src/components/Logo.jsx
@@ -0,0 +1,22 @@
+import { useId } from 'react'
+import clsx from 'clsx'
+
+export function Logomark({ invert = false, filled = false, ...props }) {
+  let id = useId()
+
+  return (
+ <p> Linus and Levi </p>
+  )
+}
+
+export function Logo({
+  className,
+  invert = false,
+  filled = false,
+  fillOnHover = false,
+  ...props
+}) {
+  return (
+    <p> . </p>
+  )
+}
